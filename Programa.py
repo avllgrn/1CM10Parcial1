e3f54f41eef2177e7b94c210718a51dede716d68
avllgrn <avillagran@ipn.mx>
2025-09-24 08:05:58 -0600

    Funcion sin argumentos, para cuadro

--- Programa.py
+++ Programa.py
@@ -1,15 +1,17 @@
 # Se define la funcion
-def sumaDeDosNumeros(primero, segundo):
-    # Se calcula la operación y se retorna el resultado
-    return primero + segundo
+def areaYPerimetroDeCuadro():
+    # Se piden datos
+    lado = float(input('Ingresa el lado del cuadro '))
+
+    # Se calcula la operación
+    area = lado**2
+    perimetro = 4*lado
+
+    # Se muestra el resultado
+    print(f'lado = {lado}')
+    print(f'area = {area}')
+    print(f'perimetro = {perimetro}')
 
 
-# Se piden datos
-primero = float(input('Ingresa un número '))
-segundo = float(input('Ingresa otro número '))
-
-# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
-resultado = sumaDeDosNumeros(primero, segundo)
-
-# Se muestra el resultado
-print(f'{primero} + {segundo} = {resultado}')
+# Se invoca la funcion
+areaYPerimetroDeCuadro()
