f4a7d39caf4ee181b9d6604187cf2d3c687e1960
avllgrn <avillagran@ipn.mx>
2025-09-24 10:05:12 -0600

    Funcion sin argumentos, para menor de edad (if-else)

--- Programa.py
+++ Programa.py
@@ -1,15 +1,12 @@
 # Se define la funcion
-def ifMenorDeEdad(edad):
-    # Se calcula la operación y se retorna el resultado
+def ifMenorDeEdad():
+    edad = int(input('Ingresa tu edad '))
+
     if edad < 18:
-        return 'Eres menor de edad'
+        print('Eres menor de edad')
+    else:
+        print('NO eres menor de edad')
 
 
-# Se piden datos
-edad = int(input('Ingresa tu edad '))
-
-# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
-mensaje = ifMenorDeEdad(edad)
-
-# Se muestra el resultado
-print(f'{mensaje}')
+# Se invoca la funcion
+ifMenorDeEdad()
