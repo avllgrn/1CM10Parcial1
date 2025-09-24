42f0584ed6fba1e45d6946eb48f8ef81715b8d64
avllgrn <avillagran@ipn.mx>
2025-09-24 08:51:03 -0600

    Funcion con argumentos, para funciones trigonometricas

--- Programa.py
+++ Programa.py
@@ -1,21 +1,42 @@
 import math
 
 # Se define la funcion
-def funcionesTrigonometricas():
-    # Se piden datos
-    grados = float(input('Ingresa los grados '))
-
+def senoDe(grados):
     # Se calcula la operación
     radianes = grados * math.pi / 180
-    seno = math.sin(radianes)
-    coseno = math.cos(radianes)
-    tangente = math.tan(radianes)
 
-    # Se muestra el resultado
-    print(f'grados = {grados}')
-    print(f'seno({grados})     = {seno}')
-    print(f'coseno({grados})   = {coseno}')
-    print(f'tangente({grados}) = {tangente}')
+    # Se calcula la operación y se retorna el resultado
+    return math.sin(radianes)
 
-# Se invoca la funcion
-funcionesTrigonometricas()
+# Se define la funcion
+def cosenoDe(grados):
+    # Se calcula la operación
+    radianes = grados * math.pi / 180
+
+    # Se calcula la operación y se retorna el resultado
+    return math.cos(radianes)
+
+# Se define la funcion
+def TangenteDe(grados):
+    # Se calcula la operación
+    radianes = grados * math.pi / 180
+
+    # Se calcula la operación y se retorna el resultado
+    return math.tan(radianes)
+
+
+# Se piden datos
+grados = float(input('Ingresa los grados '))
+
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+seno = senoDe(grados)
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+coseno = cosenoDe(grados)
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+tangente = TangenteDe(grados)
+
+# Se muestra el resultado
+print(f'grados = {grados}')
+print(f'seno({grados})     = {seno}')
+print(f'coseno({grados})   = {coseno}')
+print(f'tangente({grados}) = {tangente}')
