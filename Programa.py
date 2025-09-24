2b28fa8509ff8f87b7b6f6c59197bfc0a841ab26
avllgrn <avillagran@ipn.mx>
2025-09-24 08:14:55 -0600

    Funcion con argumentos, para rectangulo

--- Programa.py
+++ Programa.py
@@ -1,19 +1,25 @@
 # Se define la funcion
-def areaYPerimetroDeRectangulo():
-    # Se piden datos
-    base = float(input('Ingresa la base del rectangulo '))
-    altura = float(input('Ingresa la altura del rectangulo '))
+def areaDeRectangulo(base,altura):
+    # Se calcula la operación y se retorna el resultado
+    return base*altura
 
-    # Se calcula la operación
-    area = base*altura
-    perimetro = 2*base + 2*altura
-
-    # Se muestra el resultado
-    print(f'base = {base}')
-    print(f'altura = {altura}')
-    print(f'area = {area}')
-    print(f'perimetro = {perimetro}')
+# Se define la funcion
+def perimetroDeRectangulo(base,altura):
+    # Se calcula la operación y se retorna el resultado
+    return 2*base + 2*altura
 
 
-# Se invoca la funcion
-areaYPerimetroDeRectangulo()
+# Se piden datos
+base = float(input('Ingresa la base del rectangulo '))
+altura = float(input('Ingresa la altura del rectangulo '))
+
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+area = areaDeRectangulo(base,altura)
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+perimetro = perimetroDeRectangulo(base,altura)
+
+# Se muestra el resultado
+print(f'base = {base}')
+print(f'altura = {altura}')
+print(f'area = {area}')
+print(f'perimetro = {perimetro}')
