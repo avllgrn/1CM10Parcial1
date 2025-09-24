4a04b4f9b0c5147f191f113e4c61134f9f52756c
avllgrn <avillagran@ipn.mx>
2025-09-24 09:42:30 -0600

    if para menor de edad

--- Programa.py
+++ Programa.py
@@ -1,14 +1,4 @@
-# Se define la funcion
-def fahrenheitACelcius(celcius):
-    # Se calcula la operación y se retorna el resultado
-    return (fahrenheit - 32) * 5/9
+edad = int(input('Ingresa tu edad '))
 
-
-# Se piden datos
-fahrenheit = float(input('Ingresa los fahrenheit '))
-
-# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
-celcius = fahrenheitACelcius(fahrenheit)
-
-# Se muestra el resultado
-print(f'{fahrenheit}°C = {celcius}°F')
+if edad < 18:
+    print('Eres menor de edad')
