64fe805a3479e417cd87b9ac5a52ee7ee8265975
avllgrn <avillagran@ipn.mx>
2025-09-24 08:20:51 -0600

    Funcion con argumentos, para circulo

--- Programa.py
+++ Programa.py
@@ -1,18 +1,25 @@
 import math
 
 # Se define la funcion
-def areaYPerimetroDeCirculo():
-    # Se piden datos
-    radio = float(input('Ingresa el radio del círculo '))
+def areaDeCirculo(radio):
+    # Se calcula la operación y se retorna el resultado
+    return math.pi * radio**2
 
-    # Se calcula la operación
-    area = math.pi * radio**2
-    perimetro = 2 * math.pi * radio
+# Se define la funcion
+def perimetroDeCirculo(radio):
+    # Se calcula la operación y se retorna el resultado
+    return 2 * math.pi *radio
 
-    # Se muestra el resultado
-    print(f'radio = {radio}')
-    print(f'area = {area}')
-    print(f'perimetro = {perimetro}')
 
-# Se invoca la funcion
-areaYPerimetroDeCirculo()
+# Se piden datos
+radio = float(input('Ingresa el radio del círculo '))
+
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+area = areaDeCirculo(radio)
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+perimetro = perimetroDeCirculo(radio)
+
+# Se muestra el resultado
+print(f'radio = {radio}')
+print(f'area = {area}')
+print(f'perimetro = {perimetro}')
