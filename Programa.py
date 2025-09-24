48dcdee12e59e0b02953785c0e057cdf7e97701e
avllgrn <avillagran@ipn.mx>
2025-09-24 09:23:29 -0600

    Funcion sin argumentos, para Fahrenheit a Celsius

--- Programa.py
+++ Programa.py
@@ -1,14 +1,13 @@
 # Se define la funcion
-def celciusAFahrenheit(celcius):
-    # Se calcula la operación y se retorna el resultado
-    return 9/5*celcius + 32
+def celciusAfahrenheit():
+    # Se piden datos
+    fahrenheit = float(input('Ingresa los fahrenheit '))
 
+    # Se calcula la operación
+    celcius = (fahrenheit - 32) * 5/9
 
-# Se piden datos
-celcius = float(input('Ingresa los celcius '))
+    # Se muestra el resultado
+    print(f'{fahrenheit}°C = {celcius}°F')
 
-# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
-fahrenheit = celciusAFahrenheit(celcius)
-
-# Se muestra el resultado
-print(f'{celcius}°C = {fahrenheit}°F')
+# Se invoca la funcion
+celciusAfahrenheit()
