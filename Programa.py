7efefef480d0509f0af6693d7be042f9031c28fa
avllgrn <avillagran@ipn.mx>
2025-09-24 08:31:08 -0600

    Funcion sin argumentos, para radianes a grados

--- Programa.py
+++ Programa.py
@@ -1,16 +1,15 @@
 import math
 
 # Se define la funcion
-def gradosARadianes(grados):
-    # Se calcula la operación y se retorna el resultado
-    return grados * math.pi / 180
+def radianesAGrados():
+    # Se piden datos
+    radianes = float(input('Ingresa los radianes '))
 
+    # Se calcula la operación
+    grados = radianes * 180 /  math.pi
 
-# Se piden datos
-grados = float(input('Ingresa los grados '))
+    # Se muestra el resultado
+    print(f'{radianes} rad = {grados}°')
 
-# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
-radianes = gradosARadianes(grados)
-
-# Se muestra el resultado
-print(f'{grados}° = {radianes} rad')
+# Se invoca la funcion
+radianesAGrados()
