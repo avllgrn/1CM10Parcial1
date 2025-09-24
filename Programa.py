2c68dde0048cdcc6b4f4f34df69938b0b0a06485
avllgrn <avillagran@ipn.mx>
2025-09-24 08:09:45 -0600

    Funcion con argumentos, para cuadro

--- Programa.py
+++ Programa.py
@@ -1,17 +1,23 @@
 # Se define la funcion
-def areaYPerimetroDeCuadro():
-    # Se piden datos
-    lado = float(input('Ingresa el lado del cuadro '))
+def areaDeCuadro(lado):
+    # Se calcula la operación y se retorna el resultado
+    return lado**2
 
-    # Se calcula la operación
-    area = lado**2
-    perimetro = 4*lado
-
-    # Se muestra el resultado
-    print(f'lado = {lado}')
-    print(f'area = {area}')
-    print(f'perimetro = {perimetro}')
+# Se define la funcion
+def perimetroDeCuadro(lado):
+    # Se calcula la operación y se retorna el resultado
+    return 4*lado
 
 
-# Se invoca la funcion
-areaYPerimetroDeCuadro()
+# Se piden datos
+lado = float(input('Ingresa el lado del cuadro '))
+
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+area = areaDeCuadro(lado)
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+perimetro = perimetroDeCuadro(lado)
+
+# Se muestra el resultado
+print(f'lado = {lado}')
+print(f'area = {area}')
+print(f'perimetro = {perimetro}')
