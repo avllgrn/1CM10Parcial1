387b76e3592046bdba7dcf4bd5dc6611654f1a98
avllgrn <avillagran@ipn.mx>
2025-09-24 09:15:00 -0600

    Funcion sin argumentos, para Celsius a Fahrenheit

--- Programa.py
+++ Programa.py
@@ -1,42 +1,13 @@
-import math
-
 # Se define la funcion
-def senoDe(grados):
+def celciusAFahrenheit():
+    # Se piden datos
+    celcius = float(input('Ingresa los celcius '))
+
     # Se calcula la operación
-    radianes = grados * math.pi / 180
+    fahrenheit = 9/5*celcius + 32
 
-    # Se calcula la operación y se retorna el resultado
-    return math.sin(radianes)
+    # Se muestra el resultado
+    print(f'{celcius}°C = {fahrenheit}°F')
 
-# Se define la funcion
-def cosenoDe(grados):
-    # Se calcula la operación
-    radianes = grados * math.pi / 180
-
-    # Se calcula la operación y se retorna el resultado
-    return math.cos(radianes)
-
-# Se define la funcion
-def TangenteDe(grados):
-    # Se calcula la operación
-    radianes = grados * math.pi / 180
-
-    # Se calcula la operación y se retorna el resultado
-    return math.tan(radianes)
-
-
-# Se piden datos
-grados = float(input('Ingresa los grados '))
-
-# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
-seno = senoDe(grados)
-# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
-coseno = cosenoDe(grados)
-# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
-tangente = TangenteDe(grados)
-
-# Se muestra el resultado
-print(f'grados = {grados}')
-print(f'seno({grados})     = {seno}')
-print(f'coseno({grados})   = {coseno}')
-print(f'tangente({grados}) = {tangente}')
+# Se invoca la funcion
+celciusAFahrenheit()
