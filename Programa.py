c6d5a7cf3ce213c8521af56fbbd7f5964addc2c6
avllgrn <avillagran@ipn.mx>
2025-09-24 08:01:36 -0600

    Funcion con argumentos, que suma dos numeros

--- Programa.py
+++ Programa.py
@@ -1,15 +1,15 @@
 # Se define la funcion
-def sumaDeDosNumeros():
-    # Se piden datos
-    primero = float(input('Ingresa un número '))
-    segundo = float(input('Ingresa otro número '))
-
-    # Se calcula la operación
-    resultado = primero + segundo
-
-    # Se muestra el resultado
-    print(f'{primero} + {segundo} = {resultado}')
+def sumaDeDosNumeros(primero, segundo):
+    # Se calcula la operación y se retorna el resultado
+    return primero + segundo
 
 
-# Se invoca la funcion
-sumaDeDosNumeros()
+# Se piden datos
+primero = float(input('Ingresa un número '))
+segundo = float(input('Ingresa otro número '))
+
+# Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+resultado = sumaDeDosNumeros(primero, segundo)
+
+# Se muestra el resultado
+print(f'{primero} + {segundo} = {resultado}')
