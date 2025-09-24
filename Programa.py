28c4787ff853cf2de52e745051b79f2f3992c9c7
avllgrn <avillagran@ipn.mx>
2025-09-24 09:55:23 -0600

    if-else para genero Y estado civil

--- Programa.py
+++ Programa.py
@@ -1,15 +1,46 @@
+genero = input('Ingresa tu género (f/m) ')
 estadoCivil = input('Ingresa tu estado civil (s/c/d/v) ')
 
-if estadoCivil == 's':
-    print('Estado civil soltero')
-else:
-    if estadoCivil == 'c':
-        print('Estado civil casado')
+if genero == 'f':
+    if estadoCivil == 's':
+        print('Estado civil soltera')
     else:
-        if estadoCivil == 'd':
-            print('Estado civil divorciado')
+        if estadoCivil == 'c':
+            print('Estado civil casada')
         else:
-            if estadoCivil == 'v':
-                print('Estado civil viudo')
+            if estadoCivil == 'd':
+                print('Estado civil divorciada')
             else:
-                print('Estado civil no es ni soltero, ni casado, ni divorciado, ni viudo')
+                if estadoCivil == 'v':
+                    print('Estado civil viuda')
+                else:
+                    print('Género femenino, Estado civil no es ni soltero, ni casado, ni divorciado, ni viudo')
+else:
+    if genero == 'm':
+        if estadoCivil == 's':
+            print('Estado civil soltero')
+        else:
+            if estadoCivil == 'c':
+                print('Estado civil casado')
+            else:
+                if estadoCivil == 'd':
+                    print('Estado civil divorciado')
+                else:
+                    if estadoCivil == 'v':
+                        print('Estado civil viudo')
+                    else:
+                        print('Género masculino, Estado civil no es ni soltero, ni casado, ni divorciado, ni viudo')
+    else:
+        if estadoCivil == 's':
+            print('Genero no es ni femenino ni masculino, Estado civil soltero')
+        else:
+            if estadoCivil == 'c':
+                print('Genero no es ni femenino ni masculino, Estado civil casado')
+            else:
+                if estadoCivil == 'd':
+                    print('Genero no es ni femenino ni masculino, Estado civil divorciado')
+                else:
+                    if estadoCivil == 'v':
+                        print('Genero no es ni femenino ni masculino, Estado civil viudo')
+                    else:
+                        print('Genero no es ni femenino ni masculino, Estado civil no es ni soltero, ni casado, ni divorciado, ni viudo')
