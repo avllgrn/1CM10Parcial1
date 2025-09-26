650ce97479e0f41c289eb030a10b6603b6c3c6d9
avllgrn <avillagran@ipn.mx>
2025-09-26 08:16:30 -0600

    Edad con if - elif - else

--- Programa.py
+++ Programa.py
@@ -1,59 +1,10 @@
-import math
+edad = int(input('Ingresa tu edad '))
 
-# Se define la funcion
-def areaYPerimetroDeCirculo():
-    # Se piden datos
-    radio = float(input('Ingresa el radio del círculo '))
-
-    # Se calcula la operación
-    area = math.pi * radio**2
-    perimetro = 2 * math.pi * radio
-
-    # Se muestra el resultado
-    print(f'radio = {radio}')
-    print(f'area = {area}')
-    print(f'perimetro = {perimetro}')
-
-# Se define la funcion
-def gradosARadianes():
-    # Se piden datos
-    grados = float(input('Ingresa los grados '))
-
-    # Se calcula la operación
-    radianes = grados * math.pi / 180
-
-    # Se muestra el resultado
-    print(f'{grados}° = {radianes} rad')
-
-# Se define la funcion
-def funcionesTrigonometricas():
-    # Se piden datos
-    grados = float(input('Ingresa los grados '))
-
-    # Se calcula la operación
-    radianes = grados * math.pi / 180
-    seno = math.sin(radianes)
-    coseno = math.cos(radianes)
-    tangente = math.tan(radianes)
-
-    # Se muestra el resultado
-    print(f'grados = {grados}')
-    print(f'seno({grados})     = {seno}')
-    print(f'coseno({grados})   = {coseno}')
-    print(f'tangente({grados}) = {tangente}')
-
-# Menú de opciones que invoca a la función deseada por el usuario
-print('1. Área y perímetro de un Círculo')
-print('2. Conversión de grados a radianes')
-print('3. Funciones Trigonométricas')
-opcion = int(input('Qué opción quieres? ')) # Se pide la opción
-
-# La función invocada está condicionada por la opción elegida por el usuario
-if opcion == 1:
-    areaYPerimetroDeCirculo()
-elif opcion == 2:
-    gradosARadianes()
-elif opcion == 3:
-    funcionesTrigonometricas()
+if edad < 0:
+    print('Error! Edad invalida (-)')
+elif edad < 18:
+    print('Eres menor de edad')
+elif edad < 100:
+    print('Eres mayor de edad')
 else:
-    print('Opcion invalida...')
+    print('Error! Edad invalida (+)')
