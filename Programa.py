0c23025ab88080fae96b2861f80e443bd923c35d
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-09-28 14:16:23 -0600

    Menu (funcion sin argumentos sin retorno; funcion con argumentos con retorno)

--- Programa.py
+++ Programa.py
@@ -1,10 +1,49 @@
-edad = int(input('Ingresa tu edad '))
+import math
 
-if edad < 0:
-    print('Error! Edad invalida (-)')
-elif edad < 18:
-    print('Eres menor de edad')
-elif edad < 100:
-    print('Eres mayor de edad')
-else:
-    print('Error! Edad invalida (+)')
+# Se define la funcion
+def areaYPerimetroDeCirculo():
+    # Se piden datos
+    radio = float(input('Ingresa el radio del círculo '))
+
+    # Se calcula la operación
+    area = math.pi * radio**2
+    perimetro = 2 * math.pi * radio
+
+    # Se muestra el resultado
+    print(f'radio = {radio}')
+    print(f'area = {area}')
+    print(f'perimetro = {perimetro}')
+
+
+# Se define la funcion
+def gradosARadianes(grados):
+    # Se calcula la operación y se retorna el resultado
+    return grados * math.pi / 180
+
+# Se define la funcion
+def menu():
+    # Menú de opciones que invoca a la función deseada por el usuario
+    print('1. Área y perímetro de un Círculo')
+    print('2. Conversión de grados a radianes')
+    print('3. Funciones Trigonométricas')
+    opcion = int(input('Qué opción quieres? ')) # Se pide la opción
+
+    # La función invocada está condicionada por la opción elegida por el usuario
+    if opcion == 1:
+        areaYPerimetroDeCirculo()
+
+    elif opcion == 2:
+        # Se piden datos
+        grados = float(input('Ingresa los grados '))
+
+        # Se invoca la funcion, se le envían argumentos, se recibe y se guarda resultado
+        radianes = gradosARadianes(grados)
+
+        # Se muestra el resultado
+        print(f'{grados}° = {radianes} rad')
+
+    else:
+        print('Opcion invalida...')
+
+# Se invoca la función del menú
+menu()
